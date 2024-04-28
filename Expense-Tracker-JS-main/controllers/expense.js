bc40ed733471d8a6e3ce9223b1a1d90d33a5b630
Jagadeesh5106 <141550989+Jagadeesh5106@users.noreply.github.com>
2024-04-28 15:15:38 +0530

    Added edit feature for expenses

--- Expense-Tracker-JS-main/controllers/expense.js
+++ Expense-Tracker-JS-main/controllers/expense.js
@@ -2,11 +2,11 @@ const { where } = require('sequelize')
 const Expense = require('../models/expense')
 
 exports.addExpense = (req,res,next)=>{
-    console.log(req.expenseDetailsObj)
-    console.log(req.expenseObject)
-    console.log(req.json)
-    console.log(req.body);
-    console.log(req.amount);
+    // console.log(req.expenseDetailsObj)
+    // console.log(req.expenseObject)
+    // console.log(req.json)
+    // console.log(req.body);
+    // console.log(req.amount);
     const description = req.body.description;
     const type = req.body.type;
     const amount = req.body.amount;
@@ -38,12 +38,39 @@ exports.getExpenses = (req,res,next)=>{
         console.log(err)
     })
 }
-
+exports.getExpense = (req,res,next)=>{
+    const expenseId = req.params.expenseId;
+    Expense.findByPk(expenseId).then(expense=>{
+        return res.send(expense);
+    }).catch(err=>{
+        console.log("No Expense Found with ID")
+    })
+}
 exports.deleteExpense = (req,res,next)=>{
     const expenseId = req.params.expenseId;
     Expense.destroy({where:{id:expenseId}}).then(result=>{
         console.log(result);
         return res.send({message:'Deleted'})
+    }).catch(err=>{
+        console.log(err);
+    })
+}
+
+exports.editExpense = (req,res,next)=>{
+    const expenseId = req.params.expenseId;
+    const editMode = req.query.edit;
+    const updatedAmount = req.body.amount;
+    const updatedDescription = req.body.description;
+    const updatedType = req.body.type;
+    Expense.findByPk(expenseId).then(expense=>{
+        expense.amount = updatedAmount
+        expense.description = updatedDescription;
+        expense.type = updatedType;
+        return expense.save()
+    }
+    ).then(result=>{
+        console.log("Expense Updated");
+        return res.send({message:'Expense Updated'})
     }).catch(err=>{
         console.log(err);
     })
--- Expense-Tracker-JS-main/routes/expense.js
+++ Expense-Tracker-JS-main/routes/expense.js
@@ -5,5 +5,7 @@ const expenseController = require('../controllers/expense')
 
 router.post('/',expenseController.addExpense);
 router.get('/',expenseController.getExpenses);
+router.get('/:expenseId',expenseController.getExpense);
 router.delete('/:expenseId',expenseController.deleteExpense);
+router.post('/:expenseId',expenseController.editExpense);
 module.exports = router;
